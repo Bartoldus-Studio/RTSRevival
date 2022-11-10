6f142a612f9105a88e14f2fb7a2163d12bacf572
Justus <julchtwss@gmail.com>
2022-11-10 16:28:13 +0100

    Deleting all initials (chaotic shit going on)

--- Intermediate/Build/Win64/RTSRevivalEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30138
-#include "D:/Unreal Engine/RTSRevival/Intermediate/Build/Win64/RTSRevivalEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Source/RTSRevival/RTSRevival.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RTSRevival.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RTSRevival, "RTSRevival" );
